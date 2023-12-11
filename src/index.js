05fd81cc1a64780018214c945f4ea96d9ad22237
mr-wontdo <122188740+mr-wontdo@users.noreply.github.com>
2023-12-10 21:20:36 -0800

    Add logic to get forecast data via API

--- src/index.js
+++ src/index.js
@@ -12,6 +12,20 @@ const getWeatherData = async (location) => {
     }
 };
 
+// Get forecast data
+const getForecastData = async (location) => {
+    try {
+        const response = await fetch('http://api.weatherapi.com/v1/forecast.json?key=c81cf0bc1ec1474684580852233011&q=' + location, { mode: 'cors' });
+        const data = await response.json();
+        if (data.hasOwnProperty('error')) {
+            throw new Error(data.error.message);
+        }
+        return data;
+    } catch (error) {
+        console.log(error.toString().replace('Error: ', ''));
+    }
+};
+
 // Search for a specific location
 // Toggle between Fahrenheit or Celsius
 // Change the look of the page based on the data, maybe by changing the color of the background or by adding images that describe the weather.
