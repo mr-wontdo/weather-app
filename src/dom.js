241c419bdd017314f1f7b77b2503bcb66f32d6ba
mr-wontdo <122188740+mr-wontdo@users.noreply.github.com>
2023-12-11 16:29:47 -0800

    Add logic to swap between measurement systems

--- src/dom.js
+++ src/dom.js
@@ -3,17 +3,21 @@ import processData from './api';
 // Render data to HTML
 const renderData = async (searchBarValue) => {
     const data = await processData(searchBarValue);
-    console.log(data);
-
+    console.log(data); // Need to remove after testing
     document.querySelector('.location').textContent = data.location;
     document.querySelector('.local-time').textContent = data.localTime;
-    document.querySelector('.temp').textContent = data.tempC;
     document.querySelector('.condition').textContent = data.condition;
-    document.querySelector('.feels-like').textContent = data.feelsLikeC;
     document.querySelector('.chance-rain').textContent = data.chanceRain;
     document.querySelector('.humidity').textContent = data.humidity;
-    document.querySelector('.wind-speed').textContent = data.windSpeedKPH;
-
+    if (units.getSystem() === 'SI') {
+        document.querySelector('.temp').textContent = data.tempC;
+        document.querySelector('.feels-like').textContent = data.feelsLikeC;
+        document.querySelector('.wind-speed').textContent = data.windSpeedKPH;
+    } else if (units.getSystem() === 'US') {
+        document.querySelector('.temp').textContent = data.tempF;
+        document.querySelector('.feels-like').textContent = data.feelsLikeF;
+        document.querySelector('.wind-speed').textContent = data.windSpeedMPH;
+    }
     previousSearch.setValue(searchBarValue);
     document.querySelector('input').value = '';
 };
@@ -38,6 +42,29 @@ const previousSearchHandler = () => {
     return { getValue, setValue };
 };
 
+// Units handler
+const unitsHandler = () => {
+    let system = 'SI';
+    const getSystem = () => system;
+    const setSystemSI = () => {
+        system = 'SI';
+    };
+    const setSystemUS = () => {
+        system = 'US';
+    };
+    document.querySelector('button.SI').addEventListener('click', () => {
+        setSystemSI();
+        renderData(previousSearch.getValue());
+    });
+    document.querySelector('button.US').addEventListener('click', () => {
+        setSystemUS();
+        renderData(previousSearch.getValue());
+    });
+    return { getSystem };
+};
+
+// Initialization
 const previousSearch = previousSearchHandler();
+const units = unitsHandler();
 
 export default renderData;
