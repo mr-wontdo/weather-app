d961ece69d55e28595c014c4608efa662c68e1e7
mr-wontdo <122188740+mr-wontdo@users.noreply.github.com>
2023-12-25 20:02:49 -0800

    Add function to get day suffix

--- src/utils.js
+++ src/utils.js
@@ -7,4 +7,17 @@ const getDayStatus = (localTime) => {
     return false;
 };
 
+const getDaySuffix = (dd) => {
+    const suffixes = [
+    //   0     1     2     3     4     5     6     7     8     9
+        'th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th',
+    //   10    11    12    13    14    15    16    17    18    19
+        'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th',
+    //   20    21    22    23    24    25    26    27    28    29
+        'th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th',
+    //   30    31
+        'th', 'st'];
+    return suffixes[+dd];
+};
+
 export default getDayStatus;
